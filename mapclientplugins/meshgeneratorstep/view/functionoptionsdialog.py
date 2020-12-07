bca3390e2bd3e6dba5b0f0b4cbd54f23c56d339e
Richard Christie <r.christie@auckland.ac.nz>
2020-12-07 23:05:11 +1300

    Add options dialog to interactive functions

---
+++ mapclientplugins/meshgeneratorstep/view/functionoptionsdialog.py
@@ -0,0 +1,104 @@
+from PySide import QtGui, QtCore
+from functools import partial
+
+class FunctionOptionsDialog(QtGui.QDialog):
+    '''
+    Modal dialog allowing a dict of options to be edited, then OK/Cancel to be returned.
+    '''
+
+    def __init__(self, functionName, functionOptions, parent):
+        '''
+        :param functionOptions: dict of name -> value.
+        '''
+        super(FunctionOptionsDialog, self).__init__(parent)
+        self._functionName = functionName
+        self._functionOptions = functionOptions
+        self._setup()
+
+    def _setup(self):
+        self.setWindowTitle(self._functionName)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        self.setSizePolicy(sizePolicy)
+        self.setContextMenuPolicy(QtCore.Qt.NoContextMenu)
+        self._dialogLayout = QtGui.QVBoxLayout(self)
+        self._dialogLayout.setObjectName("dialogLayout")
+        self.setModal(True)
+        self._addOptions()
+        self.setWindowFlags(self.windowFlags() ^ QtCore.Qt.WindowContextHelpButtonHint)  # hide window context help (?)
+        self.resize(300, 150)
+        self._buttonBox = QtGui.QDialogButtonBox(self)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self._buttonBox.sizePolicy().hasHeightForWidth())
+        self._buttonBox.setSizePolicy(sizePolicy)
+        self._buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self._buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
+        self._buttonBox.setObjectName("buttonBox")
+        self._dialogLayout.addWidget(self._buttonBox)
+        self._buttonBox.accepted.connect(self.accept)
+        self._buttonBox.rejected.connect(self.reject)
+
+    def _optionCheckBoxClicked(self, checkBox):
+        self._functionOptions[checkBox.objectName()] = checkBox.isChecked()
+
+    def _optionRadioButtonClicked(self, radioButton):
+        key = radioButton.objectName()
+        option = self._functionOptions[key]
+        subKey = radioButton.text()
+        for tmpKey in option:
+            option[tmpKey] = (tmpKey == subKey)
+
+    def _optionLineEditChanged(self, lineEdit):
+        key = lineEdit.objectName()
+        oldValue = self._functionOptions[key]
+        text = lineEdit.text()
+        try:
+            if type(oldValue) is int:
+                newValue = int(text)
+            elif type(oldValue) is float:
+                newValue = float(text)
+            elif type(oldValue) is str:
+                newValue = str(text)
+            else:
+                assert False, 'Unimplemented type in function option dialog'
+        except:
+            print('FunctionOptionDialog: Invalid value')
+            return
+        self._functionOptions[key] = newValue
+
+    def _addOptions(self):
+        for key in self._functionOptions:
+            value = self._functionOptions[key]
+            if type(value) is bool:
+                checkBox = QtGui.QCheckBox(self)
+                checkBox.setObjectName(key)
+                checkBox.setText(key)
+                checkBox.setChecked(value)
+                callback = partial(self._optionCheckBoxClicked, checkBox)
+                checkBox.clicked.connect(callback)
+                self._dialogLayout.addWidget(checkBox)
+            else:
+                label = QtGui.QLabel(self)
+                label.setObjectName(key)
+                label.setText(key)
+                self._dialogLayout.addWidget(label)
+                if type(value) is dict:
+                    # radio buttons
+                    for subKey, subValue in value.items():
+                        radioButton = QtGui.QRadioButton(self)
+                        radioButton.setObjectName(key)
+                        radioButton.setText(subKey)
+                        radioButton.setChecked(subValue)
+                        callback = partial(self._optionRadioButtonClicked, radioButton)
+                        radioButton.clicked.connect(callback)
+                        self._dialogLayout.addWidget(radioButton)
+                else:
+                    lineEdit = QtGui.QLineEdit(self)
+                    lineEdit.setObjectName(key)
+                    lineEdit.setText(value)
+                    callback = partial(self._optionLineEditChanged, lineEdit)
+                    lineEdit.editingFinished.connect(callback)
+                    self._dialogLayout.addWidget(lineEdit)
+        spacerItem = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self._dialogLayout.addItem(spacerItem)
