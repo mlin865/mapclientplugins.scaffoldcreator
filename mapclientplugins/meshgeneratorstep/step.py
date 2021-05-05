ef99768b4a089c7015dc4df5f9b7a55edc9eb7be
Hugh Sorby <h.sorby@auckland.ac.nz>
2021-05-05 14:37:11 +1200

    Update step.py

--- mapclientplugins/meshgeneratorstep/step.py
+++ mapclientplugins/meshgeneratorstep/step.py
@@ -90,7 +90,7 @@ class MeshGeneratorStep(WorkflowStepMountPoint):
         then set:
             self._configured = True
         """
-        dlg = ConfigureDialog(QtWidgets.QApplication.activeWindow().currentWidget())
+        dlg = ConfigureDialog(QtWidgets.QApplication.activeWindow().current_widget())
         dlg.identifierOccursCount = self._identifierOccursCount
         dlg.setConfig(self._config)
         dlg.validate()
