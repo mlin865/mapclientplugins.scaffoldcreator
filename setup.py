445c359f6fc0e101b89fa17c022b174fb0af71c6
hsorby <h.sorby@auckland.ac.nz>
2021-11-04 16:38:12 +1300

    Single source version.

--- setup.py
+++ setup.py
@@ -1,9 +1,26 @@
-from setuptools import setup, find_packages
-from setuptools.command.install import install
-import os
+import codecs
 import io
+import os
+import re
+
+from setuptools import setup, find_packages
 
 SETUP_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def read(*parts):
+    with codecs.open(os.path.join(SETUP_DIR, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 
 # List all of your Python package dependencies in the
 # requirements.txt file
@@ -21,7 +38,7 @@ readme = readfile("README.rst", split=True)[3:]  # skip title
 # into the 'requirements.txt' file.
 requires = [
     # minimal requirements listing
-    "scaffoldmaker @ https://api.github.com/repos/ABI-Software/scaffoldmaker/tarball/main",
+    "scaffoldmaker @ https://api.github.com/repos/ABI-Software/scaffoldmaker/tarball/v0.2.0",
     "opencmiss.maths",
     "opencmiss.utils >= 0.3",
     "opencmiss.zinc > 3.2",
@@ -30,34 +47,23 @@ requires = [
 ]
 source_license = readfile("LICENSE")
 
-
-class InstallCommand(install):
-
-    def run(self):
-        install.run(self)
-        # Automatically install requirements from requirements.txt
-        import subprocess
-        subprocess.call(['pip', 'install', '-r', os.path.join(SETUP_DIR, 'requirements.txt')])
-
-
 setup(
     name='mapclientplugins.meshgeneratorstep',
-    version='0.1.1',
-    description='',
+    version=find_version('mapclientplugins', 'meshgeneratorstep', '__init__.py'),
+    description='MAP Client plugin for generating meshes via scripts using OpenCMISS-Zinc.',
     long_description='\n'.join(readme) + source_license,
     classifiers=[
-      "Development Status :: 3 - Alpha",
-      "License :: OSI Approved :: Apache Software License",
-      "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
     ],
-    cmdclass={'install': InstallCommand,},
     author='Richard Christie',
     author_email='',
     url='https://github.com/ABI-Software/mapclientplugins.meshgeneratorstep',
     license='APACHE',
-    packages=find_packages(exclude=['ez_setup',]),
+    packages=find_packages(exclude=['ez_setup', ]),
     namespace_packages=['mapclientplugins'],
     include_package_data=True,
     zip_safe=False,
     install_requires=requires,
-    )
+)
