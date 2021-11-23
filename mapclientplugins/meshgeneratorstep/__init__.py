c017a1cb3cf4cf6dc156abbedebd37e87344ea93
hsorby <h.sorby@auckland.ac.nz>
2021-11-23 16:10:00 +1300

    Set version to 0.3.0.

--- mapclientplugins/meshgeneratorstep/__init__.py
+++ mapclientplugins/meshgeneratorstep/__init__.py
@@ -3,7 +3,7 @@
 MAP Client Plugin
 """
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 __author__ = 'Richard Christie'
 __stepname__ = 'Mesh Generator'
 __location__ = 'https://github.com/ABI-Software/mapclientplugins.meshgeneratorstep'
