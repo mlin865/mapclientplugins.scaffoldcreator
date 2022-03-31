4398a91273294901affa3d44e44fc3985124314b
hsorby <h.sorby@auckland.ac.nz>
2022-04-01 11:19:45 +1300

    Replace XXXXXXXX in conf.py with documentation project name, replace YYYY with 2022.

--- docs/conf.py
+++ docs/conf.py
@@ -2,8 +2,8 @@
 
 # -- Project information
 
-project = 'XXXXXXXX'
-copyright = 'YYYY, University of Auckland'
+project = 'mapclienplugins.meshgeneratorstep'
+copyright = '2022, University of Auckland'
 author = 'University of Auckland'
 
 release = '0.1'
